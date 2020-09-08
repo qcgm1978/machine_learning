2b73e4192a85db0f03cf83bc005eb4e88f8eb9fc
Philipp <zhanghongliang@192.168.1.102>
2020-09-08 16:13:41 +0800

    test folder

--- test/test_SD.py
+++ test/test_SD.py
@@ -1,3 +1,8 @@
+import sys
+import os
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 from datatype import DataTypes
 import unittest
 import pandas
