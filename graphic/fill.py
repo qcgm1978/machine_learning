35de4372c0e83666ec2593773a0e5e5d2354525b
Philipp <qcgm197874@gmail.com>
2020-09-15 12:31:09 +0800

    facecolor

--- graphic/fill.py
+++ graphic/fill.py
@@ -1,17 +1,25 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-x = np.linspace(0, 1, 500)
-y = np.sin(4 * np.pi * x) * np.exp(-5 * x)
-# First, the most basic fill plot a user can make with matplotlib:
+t = np.linspace(0.0, 2.0, 201)
+s = np.sin(2 * np.pi * t)
 
-fig, ax = plt.subplots()
+# 1) RGB tuple:
+fig, ax = plt.subplots(facecolor=(.18, .31, .31))
+# 2) hex string:
+ax.set_facecolor('#eafff5')
+# 3) gray level string:
+ax.set_title('Voltage vs. time chart', color='0.7')
+# 4) single letter color string
+ax.set_xlabel('time (s)', color='c')
+# 5) a named color:
+ax.set_ylabel('voltage (mV)', color='peachpuff')
+# 6) a named xkcd color:
+ax.plot(t, s, 'xkcd:crimson')
+# 7) Cn notation:
+ax.plot(t, .7*s, color='C4', linestyle='--')
+# 8) tab notation:
+ax.tick_params(labelcolor='tab:orange')
 
-# ax.fill(x, y, zorder=10)
-ax.fill(x, y)
-# ax.grid(True, zorder=5)
 
-# x = np.linspace(0, 2 * np.pi, 500)
-# y1 = np.sin(x)
-# y2 = np.sin(3 * x)
 plt.show()
