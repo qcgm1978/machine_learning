c3b705c85b5f907f7f4780c5c8a96464e1264219
Philipp <qcgm197874@gmail.com>
2020-09-22 23:54:38 +0800

    stand form

--- test/fun/polinomials.py
+++ test/fun/polinomials.py
@@ -40,6 +40,14 @@ class TDD_POLINOMIALS(unittest.TestCase):
         self.assertFalse(b9)
         self.assertFalse(b10)
         self.assertTrue(self.p.isPolynomial('x - 2'))
+        self.assertTrue(self.p.isPolynomial(5))
+        self.assertFalse(self.p.isPolynomial('3xy**-2'))
+        self.assertFalse(self.p.isPolynomial('2/(x+2)'))
+        self.assertFalse(self.p.isPolynomial('1/x'))
+        self.assertFalse(self.p.isPolynomial('x**(1/2)'))
+        self.assertTrue(self.p.isPolynomial('x/2'))
+        self.assertTrue(self.p.isPolynomial('3x/8'))
+        self.assertTrue(self.p.isPolynomial('2**(1/2)'))
         # self.assertTrue(self.p.isPolynomial('-6y**2 - ( 7/9)x'))
         # s='''3x
         #     x - 2
@@ -52,6 +60,32 @@ class TDD_POLINOMIALS(unittest.TestCase):
         # for item in l:
         #     print(item)
         #     self.assertTrue(self.p.isPolynomial(item))
+    def test_getNomialTerms(self):
+        self.assertEqual(self.p.getNomialTerms('3xy**2'),1)
+        self.assertEqual(self.p.getNomialTerms('5x-1'),2)
+        self.assertEqual(self.p.getNomialTerms('3x+5y**2-3'),3)
+        self.assertEqual(self.p.getNomialTerms('3x+5y**2-3+4z'),4)
+        self.assertEqual(self.p.getNomialTerms('3x+5y**2-3+4z+z**20'),5)
+    def test_getVariables(self):
+        self.assertEqual(self.p.getVariables(21),0)
+        self.assertEqual(self.p.getVariables(' x4 − 2x2 + x '),['x'])
+        self.assertEqual(self.p.getVariables('xy4 − 5x2z'),['x','y','z'])
+    def test_graph_polynomial(self):
+        x=self.p.getLinspaceData(-2,2,40)
+        coefs=[1,0,-2,1,0]
+        y=self.p.getYByFunc(x,coefs)
+        self.p\
+            .plotLine(x,y)\
+            .pltCartesianCoordinate(hideAxis=True,hasLimit=True)\
+            .saveAndShow('drawFunction')
+        degree=self.p.getDegree('4x**3-x+3')
+        degree1=self.p.getDegree('-x+3')
+        degree2=self.p.getDegree('+3')
+        self.assertEqual(degree,3)
+        self.assertEqual(degree1,1)
+        self.assertEqual(degree2,0)
+        sf=self.p.getStandardForm('3x**2 − 7 + 4x**3 + x**6')
+        self.assertEqual(sf,'x**6 + 4x**3 + 3x**2 − 7')
 if __name__ == '__main__':
     unittest.main()
 
