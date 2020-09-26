4e272e875cd7d049aa1b72c903aaae75ce0df2ca
Philipp <qcgm197874@gmail.com>
2020-09-26 22:22:21 +0800

    normalize

--- test/AIWH/ml.py
+++ test/AIWH/ml.py
@@ -4,7 +4,8 @@ import numpy as np
 import pandas as pd,re
 from scipy import stats
 from sklearn import preprocessing
-
+#Using SelectKBest to get the top features!
+from sklearn.feature_selection import SelectKBest, chi2
 class Panda(object):
     def DataFrame(self,s,cols):
         l=re.split(r'\n',s)
@@ -17,11 +18,12 @@ class Panda(object):
         return df
     def to_csv(self,df,p):
         df.to_csv(p,index=False)
-    def read_csv(self,p):
+    def loadDataSet(self,p):
+        if not hasattr(self,'df'):
         #Load the data set
-        df = pd.read_csv(p)
-        self.df=df
-        return self.df
+            df = pd.read_csv(p)
+            self.df=df
+        return self
     def removeOutliers(self):
         z = np.abs(stats.zscore(self.df._get_numeric_data()))
         self.df= self.df[(z < 3).all(axis=1)]
@@ -32,17 +34,24 @@ class Panda(object):
         self.df['RainTomorrow'].replace({'No': 0, 'Yes': 1},inplace = True)
         return self
     def normalize(self):
-        df=self.df
         scaler = preprocessing.MinMaxScaler()
         # print(df[1:2].to_string(header=True))
-        scaler.fit(df)
-        df = pd.DataFrame(scaler.transform(df), index=df.index, columns=df.columns)
-        df.iloc[4:10]
+        scaler.fit(self.df)
+        self.df = pd.DataFrame(scaler.transform(self.df), index=self.df.index, columns=self.df.columns)
+        self.df.iloc[4:10]
         return self
     def preprocessingData(self,columns):
         self.df=self.df.drop(columns=columns,axis=1)
         #Removing null values
         self.df = self.df.dropna(how='any')
-        self.removeOutliers()
+        self.removeOutliers().replace().normalize()
         return self
- 
+    def explore(self):
+        df=self.df
+        X = df.loc[:,df.columns!='RainTomorrow']
+        y = df[['RainTomorrow']]
+        selector = SelectKBest(chi2, k=3)
+        selector.fit(X, y)
+        X_new = selector.transform(X)
+        print(X.columns[selector.get_support(indices=True)])
+        return self
