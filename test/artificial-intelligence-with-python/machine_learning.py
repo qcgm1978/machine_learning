68c95dd5186ceba754305663446f7848a0cb4701
Philipp <qcgm197874@gmail.com>
2020-09-26 10:40:32 +0800

    Panda

--- test/artificial-intelligence-with-python/machine_learning.py
+++ test/artificial-intelligence-with-python/machine_learning.py
@@ -5,7 +5,7 @@ import pandas as pd,re
 class Panda(object):
     def DataFrame(self,s,cols):
         l=re.split(r'\n',s)
-        rows=list(map(lambda item:re.split(r'\s+',item),l))
+        rows=list(map(lambda item:re.split(r'\s+',item.lstrip()),l))
         columns=re.split(r'\s+',cols)
         # print(rows,columns)
         df = pd.DataFrame(np.array(rows),
@@ -14,8 +14,8 @@ class Panda(object):
         return df
     def to_csv(self,df,p):
         df.to_csv(p,index=False)
-    def read_csv(self):
+    def read_csv(self,p):
         #Load the data set
-        df = pd.read_csv('/Users/zhanghongliang/Documents/machine_learning/test/artificial-intelligence-with-python/data/weatherAUS.csv')
+        df = pd.read_csv(p)
 
  
