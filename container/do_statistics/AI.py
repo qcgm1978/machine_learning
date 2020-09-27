6a79cc42fc062389d9fee7d0d91d48a5c14e51d8
Philipp <qcgm197874@gmail.com>
2020-09-27 12:06:14 +0800

    package

--- container/do_statistics/AI.py
+++
@@ -1,5 +0,0 @@
-# Machine Learning is a step into the direction of artificial intelligence (AI).
-from do_statistics.doStats import DoStats
-from mathMethods.doMath import DoMath
-class DoAI(object):
-    pass
--- main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from container.test.AIWH.test_Machine_Learning import TDD_MACHINE_LEARNING
-def main():
-    # print(add('1', '1'))
-    pass
-
-if __name__ == '__main__':
-    main()
