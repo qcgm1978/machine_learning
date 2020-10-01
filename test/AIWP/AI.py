b0bc544156f9e722fb498e0aaeb013544365c9db
Philipp <qcgm197874@gmail.com>
2020-10-01 13:26:49 +0800

    predict

--- test/AIWP/AI.py
+++ test/AIWP/AI.py
@@ -12,10 +12,27 @@ class AI(object):
         'Classification':{'type':'Supervised', 'output':'categorical' ,'aim':'category' ,'algorithm':'Logistic Regression'},
         'Clustering':{'type':'Unsupervised','output':'clusters','aim':'group','algorithm':'K-means'}
     }
+        self.normalized=False
     @property
     def shape(self):
         return self.df.shape
-   
+    @property
+    def layers(self):
+        return 3
+    def getInputOutput(self):
+        formula='y = f(X)'
+        df=self.df
+        input=[]
+        output=[]
+        for i in range (self.shape[1]):
+            data=df[:i]
+            numbers = [1, 0]
+            reports = data.isin(numbers)
+            if all(reports):
+                output.append(i)
+            else:
+                input.append(i)
+        return {'formula':formula,'input':input,'output':output}
     def getProblemCategory(self):
         return self.categories[self.category]
     def setCategory(self,category):
@@ -23,6 +40,8 @@ class AI(object):
         if self.isDL:
             self.categories[category].update({'isDL':True})
         return self
+    def setColumnsName(self,l):
+        self.columnsName=l
     def defineObjective(self,objective):
         self.objective=objective
         return self
@@ -39,11 +58,10 @@ class AI(object):
                     if not skip:
                         raise
         return self
-    def preprocessLoadData(self,p):
+    def preprocessLoadData(self,p,isPD=True):
         if not hasattr(self,'df'):
         #Load the data set
-            df = pd.read_csv(p)
-            self.df=df
+            self.df=pd.read_csv(p) if isPD else np.loadtxt(p, delimiter=',')
         return self
     def preprocessDropCols(self,columns):
         self.df=self.df.drop(columns=columns,axis=1)
@@ -55,11 +73,21 @@ class AI(object):
         self.shuffle_df = shuffle(df_sample, random_state=42)
         self.df_sample=self.valueCounts('Class',df_sample)
         return self
-    def preprocessSplit(self,featuresEnd):
+    def preprocessSplit(self,featuresEnd=None):
+        if featuresEnd is None:
+            featuresEnd=self.input_dim
         # Spilt the dataset into train and test data frame
-        endInd = int(self.n*.8)
-        df_train = self.shuffle_df[0:endInd]
-        df_test = self.shuffle_df[endInd:]
+        if hasattr(self,'n'):
+            n = self.n
+        else:
+            n=self.shape[0]
+        endInd = int(n*.8)
+        if hasattr(self,'shuffle'):
+            shuffle = self.shuffle_df
+        else:
+            shuffle=self.df
+        df_train = shuffle[0:endInd]
+        df_test = shuffle[endInd:]
         # Spilt each dataframe into feature and label
         self.train_feature = np.array(df_train.values[:, 0:featuresEnd])
         self.train_label = np.array(df_train.values[:, -1])
@@ -75,3 +103,11 @@ class AI(object):
             if abs(l[i]-l[i+1])>3000:
                 return False
         return True
+    def splitIO(self,r):
+        # split into input (X) and output (y) variables
+        # Purely integer-location based indexing for selection by position.
+        self.input = self.df.iloc[:,r]
+        self.output = self.df.iloc[:,r.stop]
+        self.input_dim=self.input.shape[1]
+        return self.input,self.output
+    
