3bc3be36a1deb9a71b30bc882caef980a588dbf9
Philipp <qcgm197874@gmail.com>
2020-10-03 21:19:00 +0800

    one_hot

--- test/keras/fit_evaluate.py
+++ test/keras/fit_evaluate.py
@@ -2,11 +2,12 @@ import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 import re
 from functools import reduce
-import keras
+import keras,tensorflow as tf
 from keras import layers
 class FitEvaluate(object):
-    def __init__(self,topDense,l):
+    def __init__(self,topDense,l,epochs):
         self.compileParam=self.getCompileParam(l)
+        self.epochs=epochs
         inputs = keras.Input(shape=(784,), name="digits")
         x = layers.Dense(64, activation="relu", name="dense_1")(inputs)
         x = layers.Dense(64, activation="relu", name="dense_2")(x)
@@ -34,44 +35,43 @@ class FitEvaluate(object):
         g=re.sub(r'[A-Z]+[a-z]+',addUnderscore,g)
         g=''.join(list(g))[1:]
         return g
-    def fitEval(self):
-        (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
-        self.cols=x_train.shape[1]
+    def custom_mean_squared_error(self,y_true, y_pred):
+        return tf.math.reduce_mean(tf.square(y_true - y_pred))
+    def hotFit(self):
+        # We need to one-hot encode the labels to use MSE
+        y_train_one_hot = tf.one_hot(self.y_train, depth=10)
+        return self.model.fit(self.x_train, y_train_one_hot, batch_size=64, epochs=1)
+    def fit(self,isHot=True):
+        (self.x_train, self.y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+        self.cols=self.x_train.shape[1]
         # Preprocess the data (these are NumPy arrays)
-        x_train = x_train.reshape(60000, 784).astype("float32") / 255
+        self.x_train = self.x_train.reshape(60000, 784).astype("float32") / 255
         self.x_test = x_test.reshape(10000, 784).astype("float32") / 255
-        y_train = y_train.astype("float32")
+        self.y_train = self.y_train.astype("float32")
         self.y_test = y_test.astype("float32")
         # Reserve 10,000 samples for validation
-        x_val = x_train[-10000:]
-        y_val = y_train[-10000:]
-        x_train = x_train[:-10000]
-        y_train = y_train[:-10000]
+        x_val = self.x_train[-10000:]
+        y_val = self.y_train[-10000:]
+        self.x_train = self.x_train[:-10000]
+        self.y_train = self.y_train[:-10000]
         self.model.compile(
-            # optimizer=keras.optimizers.RMSprop(),  # Optimizer
-            # # Loss function to minimize
-            # loss=keras.losses.SparseCategoricalCrossentropy(),
-            # # List of metrics to monitor
+            optimizer=keras.optimizers.Adam(), loss=self.custom_mean_squared_error,
             # metrics=[keras.metrics.SparseCategoricalAccuracy()],
-            # optimizer="rmsprop",
-            # loss="sparse_categorical_crossentropy",
-            # metrics=["sparse_categorical_accuracy"],
-            optimizer=self.compileParam[0],
-            loss=self.compileParam[1],
-            metrics=[self.compileParam[2]]
         )
         print("Fit model on training data")
-        history = self.model.fit(
-            x_train,
-            y_train,
+        if isHot:
+            return self.hotFit()
+        else:
+            return self.model.fit(
+            self.x_train,
+            self.y_train,
             batch_size=64,
-            epochs=2,
+            epochs=self.epochs,
             # We pass some validation for
             # monitoring validation loss and metrics
             # at the end of each epoch
             validation_data=(x_val, y_val),
         )
-        return history
     def test(self):
         # Evaluate the model on the test data using `evaluate`
         print("Evaluate on test data")
