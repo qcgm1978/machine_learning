9c86b4447e132241970efb7ed32186927460cc83
Philipp <qcgm197874@gmail.com>
2020-10-04 15:43:21 +0800

    save

--- test/keras/Intro_m.py
+++ test/keras/Intro_m.py
@@ -5,5 +5,9 @@ import numpy as np,tensorflow as tf
 class Intro(np.ndarray,tf.data.Dataset):
     def __init__(self,l):
         super(tf.data.Dataset)  
+    def getDataset(self):
+        return tf.data.Dataset
+    def getNp(self):
+        return np
     def isValidData(self,data):
         return isinstance(data,Intro) or isinstance(data,tf.data.Dataset)
