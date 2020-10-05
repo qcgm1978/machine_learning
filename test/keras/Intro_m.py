1688bb5d01fa14e7cda45f1d607f37230e244300
Philipp <qcgm197874@gmail.com>
2020-10-05 20:19:55 +0800

    functional api

--- test/keras/Intro_m.py
+++ test/keras/Intro_m.py
@@ -1,13 +1,28 @@
 import os
+import keras
+
+# from tensorflow.python.framework.tensor_util import is_tensor
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 # from utilities import getPath,parseNumber
 import numpy as np,tensorflow as tf
-class Intro(np.ndarray,tf.data.Dataset):
+class Intro(np.ndarray,tf.data.Dataset,tf.keras.utils.Sequence):
     def __init__(self,l):
         super(tf.data.Dataset)  
+    def get_keras(self):
+        return tf.keras
     def getDataset(self):
         return tf.data.Dataset
+    def getSequence(self):
+        return tf.keras.utils.Sequence
     def getNp(self):
         return np
+    def get_tf(self):
+        return tf
     def isValidData(self,data):
-        return isinstance(data,Intro) or isinstance(data,tf.data.Dataset)
+        return isinstance(data,Intro) or isinstance(data,tf.data.Dataset) or isinstance(data,tf.keras.utils.Sequence)
+    def isLayer(self,layer):
+        return isinstance(layer,tf.keras.layers.Layer)
+    def is_tensor(self,tensor):
+        return isinstance(tensor,tf.Tensor)
+    def is_model(self,model):
+        return isinstance(model,keras.Model)
--- utilities.py
+++ utilities.py
@@ -1,5 +1,6 @@
 import  os
 import re
+import json
 def parseNumber(value, as_int=False):
     try:
         number = float(re.sub('[^.\-\d]', '', value))
@@ -14,3 +15,10 @@ def getPath(file,chdir=None):
     pre = os.getcwd()
     file = os.path.join(pre, file)
     return file
+def dump_json(p,duration):
+    with open(p, "r") as jsonFile:
+        data = json.load(jsonFile)
+    data.append(round(duration))
+    with open(p, "w") as jsonFile:
+        json.dump(data, jsonFile)
+    return data
