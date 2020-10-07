fc59dde92a035ef3ee185dcff834f37388980a91
Philipp <qcgm197874@gmail.com>
2020-10-08 02:13:56 +0800

    pytoch

--- test/pytoch/test_pytoch.py
+++ test/pytoch/test_pytoch.py
@@ -8,8 +8,19 @@ class TDD_PYTOCH(unittest.TestCase):
     def setUpClass(cls):
         cls.foo = 1
     def test_pytoch(self):
-        self.assertTrue(is_t)
-        self.assertTrue(is_t2)
+        gs=GetStarted()
+        gs.Tensors()
+        self.assertTrue(gs.is_t)
+        self.assertTrue(gs.is_t2)
+        self.assertEqual(gs.x3.shape,(5,3))
+        flat_list = [not item for sublist in gs.x3 for item in sublist]
+        self.assertTrue(all(flat_list))
+        self.assertTrue(gs.is_f)
+        self.assertTrue(gs.is_f1)
+        self.assertTrue(gs.is_f2)
+        self.assertEqual(gs.x5.shape,(5,3))
+        self.assertEqual(gs.x6.shape,(5,3))
+        self.assertEqual(list(gs.x6.size()),[5,3])
+
 if __name__ == '__main__':
     unittest.main()
-                
