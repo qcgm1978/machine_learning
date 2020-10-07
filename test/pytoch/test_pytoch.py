565bf3d5184496a2b890d82398fd1fed2fa42951
Philipp <qcgm197874@gmail.com>
2020-10-08 00:49:30 +0800

    pytoch

---
+++ test/pytoch/test_pytoch.py
@@ -0,0 +1,15 @@
+#import os,time
+# os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+import unittest
+# from utilities import getPath,parseNumber,update_json
+from .what_is_pytoch import *
+class TDD_PYTOCH(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.foo = 1
+    def test_pytoch(self):
+        self.assertTrue(is_t)
+        self.assertTrue(is_t2)
+if __name__ == '__main__':
+    unittest.main()
+                
