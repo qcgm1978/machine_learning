e2632ee23054f0e73352c5a98238921a7c579c0f
Philipp <qcgm197874@gmail.com>
2020-10-11 11:11:07 +0800

    tan

--- test/calculus/calculus.py
+++ test/calculus/calculus.py
@@ -1,4 +1,6 @@
+import math
 from sympy import *
+import random
 class Calculus(object):
     def is_read(self):
         return 'the integral from a to b of f-of-x with respect to x'
@@ -9,6 +11,8 @@ class Calculus(object):
             return False
     def get_ratio(self):
         return 'dy/dx'
+    def get_limite_shorthand(self):
+        return self.get_ratio(),'dy with respect to x'
     def get_replaced_sign(self):
         return ('ξ','Δ')
     def get_Lagrange_notation(self):
@@ -20,4 +24,15 @@ class Calculus(object):
         x = symbols('x')
         derivative=diff(x**2,x)
         return derivative.evalf(subs={x: a})
-        
+    def doubling_function(self,a):
+        return a*2
+    def differentiation_operator(self,f):
+        r=random.random()
+        if f(r)==r**2:
+            return lambda x:x*2
+    def get_distance_v_constants(self,v,t):
+        return v*t
+    def coin_set(self,h,d,truncate):
+        tg1 = 0.01746
+        power=10**truncate
+        return math.floor(h/d/tg1*power)/power
