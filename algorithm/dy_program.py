525b0730dbd45cb41639de1440c5a131a8b255cc
Philipp <qcgm197874@gmail.com>
2020-10-19 22:14:38 +0800

    O(n) time

--- algorithm/dy_program.py
+++ algorithm/dy_program.py
@@ -1,4 +1,18 @@
-def fib(n):
-    if n <= 1:
-        return n
-    return fib(n - 1) + fib(n - 2)
+class DP(object):
+    two_count = 0
+    m={'0':0,'1':1}
+
+    def fib(self, n):
+        self.two_count = 0
+        val = self.get_fib(n)
+        return val
+    # requires only O(n) time instead of exponential time (but requires O(n) space)
+    def get_fib(self,n):
+        s_n=str(n)
+        if s_n not in self.m:
+            if n==2:
+                self.two_count += 1
+            p = self.get_fib(n - 1)
+            q = self.get_fib(n - 2)
+            self.m[s_n]=p+q
+        return self.m[s_n]
--- algorithm/test_Dynamic_programming.py
+++ algorithm/test_Dynamic_programming.py
@@ -1,12 +1,24 @@
 import unittest
 # from utilities import getPath,parseNumber,update_json
 from dy_program import *
+
+
 class TDD_DYNAMIC_PROGRAMMING(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.foo = 1
+
     def test_Dynamic_programming(self):
-        self.assertEqual(fib(5),5)
+        d = DP()
+        fib = d.fib
+        self.assertEqual(fib(5), 5)
+        self.assertEqual(d.two_count,1)
+        self.assertEqual(fib(4) + fib(3), fib(5), 5)
+        self.assertEqual((fib(3) + fib(2)) + (fib(2) + fib(1)), fib(5), 5)
+        self.assertEqual(((fib(2) + fib(1)) + (fib(1) + fib(0))) +
+                         ((fib(1) + fib(0)) + fib(1)), fib(5), 5)
+        self.assertEqual((((fib(1) + fib(0)) + fib(1)) + (fib(1) +
+                                                          fib(0))) + ((fib(1) + fib(0)) + fib(1)), fib(5), 5)
+
 if __name__ == '__main__':
     unittest.main()
-                
